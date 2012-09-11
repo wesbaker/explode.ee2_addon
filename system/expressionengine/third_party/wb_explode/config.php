e6cb2264d503a0bc27f90ef4e9170d7f583e02ae
brett <brett@madhouse.org>
2012-09-11 16:16:06 -0700

    update to description
    removed specific pipe callout

--- system/expressionengine/third_party/wb_explode/config.php
+++ system/expressionengine/third_party/wb_explode/config.php
@@ -4,7 +4,7 @@ if (! defined('EXPLODE_NAME'))
 {
 	define('EXPLODE_NAME', 'WB Explode');
 	define('EXPLODE_VER',  '1.2.1');
-	define('EXPLODE_DESC', 'Given a string, a value to look for, and optionally an offset, will take a string separated by pipes (e.g. |) and return the first or first value or the odd or even values.');
+	define('EXPLODE_DESC', 'Given a string, a value to look for, and optionally an offset, will take a string separated by a delimiter (e.g. |) and return the first or first value or the odd or even values.');
 	define('EXPLODE_DOCS', 'http://github.com/wesbaker/wb.explode.ee2_addon');
 	define('EXPLODE_AUTHOR', 'Wes Baker');
 	define('EXPLODE_URL', 'http://github.com/wesbaker/wb.explode.ee2_addon');
